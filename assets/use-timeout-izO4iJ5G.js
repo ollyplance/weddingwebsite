83088037356df9f92603f2386bec77b2b98a61b1
Oliver Lance <ollyplance@gmail.com>
2025-11-16 18:15:59 -0800

    7fb0d29

---
+++ assets/use-timeout-izO4iJ5G.js
@@ -0,0 +1,1 @@
+import{a as u}from"./vm-D7Iaa8vV.js";import{X as i,f as m,g as s}from"./index-D7aqh5iC.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
