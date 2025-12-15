48d764fbb1a98e4ce6a6d652d385d2657ac1590b
Oliver Lance <ollyplance@gmail.com>
2025-12-14 19:52:27 -0800

    325877c

---
+++ assets/use-timeout-BcNgmtg5.js
@@ -0,0 +1,1 @@
+import{a as u}from"./vm-CsK462w-.js";import{_ as i,b as m,g as s}from"./index-BgPE9Vr7.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
