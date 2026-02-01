e9c4fa62fa39bf79c816bfce33c7448f968a0a8f
Oliver Lance <ollyplance@gmail.com>
2026-02-01 14:59:29 -0800

    0193cfa

---
+++ assets/use-timeout-k2DgFdWG.js
@@ -0,0 +1,1 @@
+import{a as u}from"./vm-HRhwR_mR.js";import{_ as i,b as m,g as s}from"./index-Bz17jKca.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
